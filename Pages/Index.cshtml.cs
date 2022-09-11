372dfa537a3e29e9775a8c707ad75af3d8572725
Karthik Chintala <karthikreddy509@gmail.com>
2022-09-11 07:42:39 +0530

    Feature magement
    feature management

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+
+namespace FeatureManagement.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IFeatureManager _featureManager;
+        public bool SecretFeatureEnabled { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, IFeatureManager featureManager)
+        {
+            _logger = logger;
+            _featureManager = featureManager;
+        }
+
+        public async void OnGet()
+        {
+            SecretFeatureEnabled = await _featureManager.IsEnabledAsync(Constants.SecreteFeature);
+            _logger.LogInformation($"Secret Feature value: {SecretFeatureEnabled}");
+        }
+    }
+
+    public static class Constants
+    {
+        public static readonly string SecreteFeature = nameof(SecreteFeature);
+    }
+}
